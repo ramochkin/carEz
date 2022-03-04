bc0b78d5476bf3e0c51a1fcecfcc97f7bbba2a9a
Brooks Rosenberg <bjtr@bu.edu>
2022-03-04 17:36:30 -0500

    actual seeds done

---
+++ seeds/model-seeds.js
@@ -0,0 +1,64 @@
+const { Model } =require('../models');
+
+const modelData = [
+    {
+        modelName: 'ILX',
+        brandId: 1,
+    },
+    {
+        modelName: 'MDX',
+        brandId: 1,
+    },
+    {
+        modelName: 'NSX',
+        brandId: 1,
+    },
+    {
+        modelName: 'Giulia',
+        brandId: 2,
+    },
+    {
+        modelName: 'Stelvio',
+        brandId: 2,
+    },
+    {
+        modelName: 'Vantage',
+        brandId: 3,
+    },
+    {
+        modelName: 'DB11',
+        brandId: 3,
+    },
+    {
+        modelName: 'DB9',
+        brandId: 3,
+    },
+    {
+        modelName: 'A5',
+        brandId: 4,
+    },
+    {
+        modelName: 'R8',
+        brandId: 4,
+    },
+    {
+        modelName: 'A7',
+        brandId: 4,
+    },
+    {
+        modelName: 'Mulsanne',
+        brandId: 5,
+    },
+    {
+        modelName: 'Flying Spur',
+        brandId: 5,
+    },
+    {
+        modelName: 'Bentayga',
+        brandId: 5,
+    },
+];
+
+const seedModels = () => Model.bulkCreate(modelData);
+
+module.exports = seedModels;
