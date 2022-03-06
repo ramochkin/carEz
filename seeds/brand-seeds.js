1086ddd0bcb92ad4e86b57dd7c2422b72576cc90
Brooks Rosenberg <bjtr@bu.edu>
2022-03-05 23:27:29 -0500

    single db for cars

--- seeds/brand-seeds.js
+++ seeds/brand-seeds.js
@@ -2,13 +2,28 @@ const { Brands } =require('../models');
 
 const brandData = [
     {
-        brand_name: 'Acura',
+        brand_name: 'Acura ILX',
     },
     {
-        brand_name: 'Alfa-Romeo',
+        brand_name: 'Acura MDX',
     },
     {
-        brand_name: 'Aston-Martin',
+        brand_name: 'Acura NSX',
+    },
+    {
+        brand_name: 'Alfa-Romeo Giulia',
+    },
+    {
+        brand_name: 'Alfa-Romeo Stelvio',
+    },
+    {
+        brand_name: 'Aston-Martin Vantage',
+    },
+    {
+        brand_name: 'Aston-Martin DB11',
+    },
+    {
+        brand_name: 'Aston-Martin DB11',
     },
 ];
 
