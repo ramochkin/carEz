1c9ecc189c1d3a7e2aab112ac529f9b378534cb8
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-03-07 11:18:05 -0500

    fixed login err and navbar in main

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,7 +7,7 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
-      userId: req.session.userId,
+      user_id: req.session.user_id,
     });
     res.json(newComment);
   } catch (err) {
